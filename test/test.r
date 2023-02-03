338617fc098d92e9b39ab6d239d386ec0b22879e
Sooyong Lee <sooyongl09@utexas.edu>
2023-02-03 13:37:58 -0600

    Update test.r

--- test/test.r
+++ test/test.r
@@ -1,19 +1,21 @@
 library(BMIMIC)
 library(rstan)
 
-stan_data <-
-  mkData(N1         = 500, 
-         N2         = 500, 
-         nitem      = 10, 
-         mu_2       = 0.2, 
+generated_data <-
+  mkData(N1         = 500,
+         N2         = 500,
+         nitem      = 10,
+         mu_2       = 0.2,
          var_2      = 2,
-         uni_dif    = c(0,0,0,0,0.2,0.2,0.2,0,0,0), 
-         nonuni_dif = c(0,0,0,0,0,0,0,0.2,0.2,0.2), 
+         uni_dif    = c(0,0,0,0,0.2,0.2,0.2,0,0,0),
+         nonuni_dif = c(0,0,0,0,0,0,0,0.2,0.2,0.2),
          anchor_n   = 4)
 
-
-stan_fit <- 
+stan_fit <-
   rstan::stan(
-    file = system.file("inst/stan", package = "BMIMIC")
-    data   = stan_data
+    file = file.path(system.file("stan", package = "BMIMIC"),
+                     "BMIMIC_example.stan"),
+    data   = generated_data$stan_data
   )
+
+summary(stan_fit)
